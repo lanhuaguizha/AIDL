72222936bcee5b5d189e67058689dd0d85a0864f
paultao <paultao@pptv.com>
2018-01-01 15:27:09 +0800

    Initial commit
    Change-Id: I192cddb90401c2dc79e8ca83d3ef8033df582d15

---
+++ app/src/main/java/com/example/paultao/aidl/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.paultao.aidl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// https://www.jianshu.com/p/d1fac6ccee98
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/paultao/aidl/MyService.java
@@ -0,0 +1,24 @@
+package com.example.paultao.aidl;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    class MyBinder extends IMyAidlInterface.Stub {
+
+        @Override
+        public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, String aString) throws RemoteException {
+        }
+    }
+}
